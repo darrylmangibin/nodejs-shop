0c4aa21ee7c9d26fcd1f0482d4446743a93d904c
Darryl <darrylmangibin@gmail.com>
2019-02-01 16:37:06 +0800

    add html code

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-const http = require('http')
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
 
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop')
+
 app.use(bodyParser.urlencoded());
 
-app.use('/add-product', (req, res, next) => {
-    res.send(`<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Send</button></form>`);
-})
+app.use('/admin', adminRoute);
+app.use(shopRoute);
 
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    res.send(`<h1>Hello from express</h1>`);
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>Page not found</h1>`)
 })
 
 app.listen(3000, () => {
